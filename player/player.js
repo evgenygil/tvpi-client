7b47d45d3676bc5b4b1c130de8277f838a1e0c7e
Evgeny Gil <eogil@yamoney.ru>
2020-08-25 21:06:18 +0300

    fix stat sending

--- player/player.js
+++ player/player.js
@@ -1,20 +1,20 @@
 (async () => {
-	const playIndex = (index) => {
-		player.src = data[index];
-		player.play();
-	};
+	const player = document.getElementById('player');
 
 	const {data} = await axios.get('http://0.0.0.0:9090/player/list');
 
-	const player = document.getElementById('player');
+	const playIndex = (index) => {
+		player.src = data[index];
+		player.play();
+
+		axios.post('http://0.0.0.0:9090/statistics/query', {media: data[activeVideo]});
+	};
+
 	let activeVideo = 0;
 	playIndex(activeVideo);
 
 	player.addEventListener('ended', () => {
 		activeVideo = (++activeVideo) % data.length;
-
-		axios.post('http://0.0.0.0:9090/statistics/query', {media: data[activeVideo]});
-
 		playIndex(activeVideo);
 	});
 })();
